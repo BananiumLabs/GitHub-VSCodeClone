fe0a3eaec9b9789b44edd32fd4c9230738467eb9
Eric Qian <eric@enumc.com>
2020-07-12 20:06:00 -0700

    add: Version 1.1.0 | fix: new GitHub style

--- content.js
+++ content.js
@@ -1,17 +1,24 @@
 // LIVE
 
-console.log("Successfully loaded GitHub VSCode Rewrite extension");
+document.querySelectorAll("span.d-none.d-md-flex")[1].addEventListener("click", function(){
+    var vscodeBTN = document.querySelectorAll('.get-repo-modal')[0].querySelectorAll('.d-flex')[1];
+    var cloneURL = document.querySelectorAll(".input-group")[1].querySelector(".form-control").value;
+    // vscodeBTN.setAttribute('data-open-app', "visual-studio-code");
 
-var vscodeBTN = document.querySelectorAll('.get-repo-btn')[1];
-var cloneURL = document.querySelectorAll(".input-group")[1].querySelector(".form-control").value;
-vscodeBTN.setAttribute('data-open-app', "visual-studio-code");
+    // var currentLabel = vscodeBTN.getAttribute('aria-label');
+    // currentLabel = currentLabel.substring(0, currentLabel.length - 1);
+    // currentLabel = currentLabel + " Code.";
+    // vscodeBTN.setAttribute('aria-label', currentLabel);
 
-var currentLabel = vscodeBTN.getAttribute('aria-label');
-currentLabel = currentLabel.substring(0, currentLabel.length - 1);
-currentLabel = currentLabel + " Code.";
-vscodeBTN.setAttribute('aria-label', currentLabel);
+    while (!vscodeBTN.innerHTML.includes("Visual Studio")) {
+    }
+    if(!vscodeBTN.innerHTML.endsWith("Code")){
+        console.log(vscodeBTN.innerHTML);
+        vscodeBTN.innerHTML = vscodeBTN.innerHTML + " Code";
+        console.log(vscodeBTN.innerHTML);
+    }
+    
+    protocolURL = "vscode://vscode.git/clone?url=" + encodeURI(cloneURL);
+    vscodeBTN.href = protocolURL;
+});
 
-vscodeBTN.innerText = vscodeBTN.innerText + " Code";
-
-protocolURL = "vscode://vscode.git/clone?url=" + encodeURI(cloneURL);
-vscodeBTN.href = protocolURL;
